dc929683a78b8ea4d832f108cee8f82a70ef2ccc
wanghuajian620 <154659041@qq.com>
2020-04-11 01:58:22 +0800

    go upgrade ，了解 post请求，以及 请求中的 controller、model、中间件

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
- r := gin.Default() // := 是简短声明 相当于 var r = gin.Default()
- r.GET("/ping", func(c *gin.Context) { // *gin 是什么，*gin.Context是参数类型
-  c.JSON(200, gin.H{
-   "message": "pong",
-  })
- })
- r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	r.POST("/post", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+		c.XML(200, gin.H{
+			"id":      id,
+			"page":    page,
+			"name":    name,
+			"message": message,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
